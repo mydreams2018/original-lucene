19c14745b95b7329f2e203e0e7a390c429c8284f
bigben <bigben@886.com>
2019-04-02 15:55:16 +0800

    笔记本2

--- src/test/java/cn/kungreat/original/OriginalApplicationTests.java
+++ src/test/java/cn/kungreat/original/OriginalApplicationTests.java
@@ -2,15 +2,24 @@ package cn.kungreat.original;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ClassUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OriginalApplicationTests {
 
+    @Value("${lucene.path}")
+    private String lucenePath;
+
     @Test
     public void contextLoads() {
+        // 获得当前运行项目路径
+        String path = ClassUtils.getDefaultClassLoader().getResource("").getPath();
+        System.out.println(path);
     }
+
 
 }
