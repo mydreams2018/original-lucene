924c8e48f76faabd369267c3a566351e68bf15ed
bigben <bigben@886.com>
2019-04-05 19:53:50 +0800

    高亮完成

--- src/test/java/cn/kungreat/original/OriginalApplicationTests.java
+++ src/test/java/cn/kungreat/original/OriginalApplicationTests.java
@@ -1,9 +1,14 @@
 package cn.kungreat.original;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
+import org.apache.lucene.search.highlight.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
@@ -19,6 +24,7 @@ import org.springframework.util.ClassUtils;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.file.Paths;
 
 @RunWith(SpringRunner.class)
@@ -77,6 +83,7 @@ public class OriginalApplicationTests {
          FieldType fieldType = new FieldType();
          fieldType.setIndexOptions(IndexOptions.DOCS);
          fieldType.setStored(true);
+        new Field("name","value",fieldType);
 
         IndexOptions.DOCS  只有文档会被索引，词频和位置都会被省略
         DOCS_AND_FREQS  文档和词频被索引，位置省略
@@ -91,6 +98,7 @@ public class OriginalApplicationTests {
         storeTermVectorPositions  向量偏移
         storeTermVectorPayloads： 是否存储Payloads信息。
         omitNorms：  是否存储norm信息，用于评分Boost值和会处理。
+
     */
     
     @Test
@@ -110,8 +118,10 @@ public class OriginalApplicationTests {
             doc.add(new StringField("title", "java", Field.Store.YES));
             doc.add(new TextField("content", "java 是专为大规模数据处理而设计的快速通用的计算引擎", Field.Store.YES));
             // DocValues 类型不分词  会创建一个排序的表  用来做排序 很快
-            doc.add(new SortedDocValuesField("id",new BytesRef("150")));
-            doc.add(new StringField("id", "150",Field.Store.YES));
+            doc.add(new SortedDocValuesField("id",new BytesRef("180")));
+            doc.add(new StringField("id", "180",Field.Store.YES));
+
+            doc.add(new IntPoint("bk", 136));
             indexWriter.addDocument(doc);
             indexWriter.commit();
         } catch (IOException e) {
@@ -127,11 +137,155 @@ public class OriginalApplicationTests {
         }
     }
 
-    @Test
+    @Test           //不分词
     public void termQueryTest() throws IOException {
-        TermQuery query = new TermQuery(new Term("id", "150"));
+        TermQuery query = new TermQuery(new Term("title", "java"));
         //执行查询，并打印查询到的记录数
         executeQuery(query);
+    }
+
+    @Test           //分词
+    public void queryParser(){
+        QueryParser queryParser = new QueryParser("title", analyzer);
+        queryParser.setDefaultOperator(QueryParser.Operator.AND);  // 查询值不分词
+        try {
+            Query query = queryParser.parse("c");
+            executeQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test           //多field 分词
+    public void multiFieldQueryParser(){
+        String fields[] = {"title","content"};
+        MultiFieldQueryParser queryParser = new MultiFieldQueryParser(fields, analyzer);
+        try {
+            Query query = queryParser.parse("c");
+            executeQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test           //boolQuery query 拼接
+    public void boolQuery(){
+
+        try {
+            String fields[] = {"title","content"};
+            MultiFieldQueryParser queryParser = new MultiFieldQueryParser(fields, analyzer);
+            Query query1 = queryParser.parse("c");
+
+            Query query2 = new TermQuery(new Term("content","nono"));
+
+            BooleanClause booleanClause1 = new BooleanClause(query1, BooleanClause.Occur.MUST);
+            BooleanClause booleanClause2 = new BooleanClause(query2, BooleanClause.Occur.SHOULD);
+            BooleanQuery booleanQuery = new BooleanQuery.Builder().add(booleanClause1).add(booleanClause2).build();
+
+            executeQuery(booleanQuery);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test   // 需要保存一个  doc.add(new IntPoint("bk", 136));  用来快速过滤定位 [不会保存在词库 查不到]
+    public void rangQuery(){
+        Query query = IntPoint.newRangeQuery("bk", 100, 160);
+        try {
+            executeQuery(query);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test   //prefixQuery
+    public void prefixQuery(){
+        Term term = new Term("content", "j");
+        PrefixQuery prefixQuery = new PrefixQuery(term);
+        try {
+            executeQuery(prefixQuery);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void PhraseQuery()  {
+        PhraseQuery.Builder builder = new PhraseQuery.Builder();
+        builder.add(new Term("content","c"));
+        builder.add(new Term("content","original"));
+        //设置分词之间的距离   content：c is original
+        builder.setSlop(1);
+        PhraseQuery phraseQuery = builder.build();
+        try {
+            executeQuery(phraseQuery);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test  //FuzzyQuery是一种模糊查询，它可以简单地识别两个相近的词语。
+    public void fuzzyQuery() {
+        Term term = new Term("content", "大规模");
+        Query query = new FuzzyQuery(term);
+        //执行查询，并打印查询到的记录数
+        try {
+            executeQuery(query);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 通配符搜索
+     * Lucene也提供了通配符的查询，这就是WildcardQuery。
+     * 通配符“?”代表1个字符，而“*”则代表0至多个字符。
+     */
+    @Test
+    public void wildcardQuery() {
+        Term term = new Term("content", "大?模");
+        Query query = new WildcardQuery(term);
+        //执行查询，并打印查询到的记录数
+        try {
+            executeQuery(query);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void highLight(){
+        //指定搜索字段和分析器
+        QueryParser parser = new QueryParser("content", analyzer);
+        //用户输入内容
+        Query query = null;
+        try {
+            query = parser.parse("java");
+            // 关键字高亮显示的html标签，需要导入lucene-highlighter-xxx.jar
+            QueryScorer queryScorer = new QueryScorer(query,"content");
+            SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<span style='color:red'>", "</span>");
+            Highlighter highlighter = new Highlighter(simpleHTMLFormatter,queryScorer);
+            TopDocs topDocs = indexSearcher.search(query, 10);
+            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+                System.out.println("分数" + scoreDoc.score);
+                System.out.println("编号" + scoreDoc.doc);
+                //取得对应的文档对象
+                Document document = indexSearcher.doc(scoreDoc.doc);
+                System.out.println("id：" + document.get("id"));
+                System.out.println("title：" + document.get("title"));
+                System.out.println("content：" + document.get("content"));
+                System.out.println("bk：" + document.get("bk"));
+                System.out.println("------------------------------------");
+                // 内容增加高亮显示
+                TokenStream tokenStream = analyzer.tokenStream("content", new StringReader(document.get("content")));
+                SimpleSpanFragmenter simpleSpanFragmenter = new SimpleSpanFragmenter(queryScorer);
+                highlighter.setTextFragmenter(simpleSpanFragmenter);
+                String content = highlighter.getBestFragment(tokenStream, document.get("content"));
+                System.out.println(content);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void executeQuery(Query query) throws IOException {
@@ -139,14 +293,14 @@ public class OriginalApplicationTests {
         //打印查询到的记录数
         System.out.println("总共查询到" + topDocs.totalHits + "个文档");
         for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-            //取得对应的文档对象
             System.out.println("分数" + scoreDoc.score);
-            // 从0开始
             System.out.println("编号" + scoreDoc.doc);
+            //取得对应的文档对象
             Document document = indexSearcher.doc(scoreDoc.doc);
             System.out.println("id：" + document.get("id"));
             System.out.println("title：" + document.get("title"));
             System.out.println("content：" + document.get("content"));
+            System.out.println("bk：" + document.get("bk"));
         }
     }
 
@@ -161,9 +315,7 @@ public class OriginalApplicationTests {
             // 删除title中含有关键词“Spark”的文档
             long count = indexWriter.deleteDocuments(new Term("title", "Spark"));
             // DeleteDocuments(Term term ...):根据Term来删除单个或多个Document
-
             // DeleteDocuments(Query query ...):根据Query条件来删除单个或多个Document
-
             // DeleteAll():删除所有的Document
 
             //使用IndexWriter进行Document删除操作时，文档并不会立即被删除，而是把这个删除动作缓存起来，
